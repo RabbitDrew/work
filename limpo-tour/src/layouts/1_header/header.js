808f0ef3425ec1a30deaba0207fc4793f289cc08
Andrew <andrei.sp.93@gmail.com>
2024-12-31 23:41:06 +0300

    feat: add media query for header block add burger btn  add burger menu  working on media query in intro block

--- limpo-tour/src/layouts/1_header/header.js
+++ limpo-tour/src/layouts/1_header/header.js
@@ -1,1 +1,4 @@
 import './style/header.css'
+import './style/header-adopt-menu.css'
+import './style/adopt-menu-animation.css'
+import './style/media-header.css'
--- limpo-tour/src/layouts/2_intro/intro.js
+++ limpo-tour/src/layouts/2_intro/intro.js
@@ -1,3 +1,5 @@
 import './style/intro--main-section-style.css'
 import './style/title.css'
 import './style/form.css'
+
+import './style/medai-intro.css'
