325f4f332fe0dc945310ada08321e4dd2382d0d0
Andrew <andrei.sp.93@gmail.com>
2025-02-15 16:14:37 +0300

    feat: add media query

--- limpo-tour/src/layouts/1_header/header.js
+++ limpo-tour/src/layouts/1_header/header.js
@@ -1,4 +1,2 @@
 import './style/header.css'
-import './style/header-adopt-menu.css'
-import './style/adopt-menu-animation.css'
-import './style/media-header.css'
+
--- limpo-tour/src/layouts/2_intro/intro.js
+++ limpo-tour/src/layouts/2_intro/intro.js
@@ -1,5 +1,2 @@
 import './style/intro--main-section-style.css'
-import './style/title.css'
-import './style/form.css'
 
-import './style/medai-intro.css'
