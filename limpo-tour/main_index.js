a8b3a8fa4eb89cee8f7f20f8f29b6dfd347b0cf1
Andrew <andrei.sp.93@gmail.com>
2025-02-16 14:15:23 +0300

    feat: add styles for expanding lists

--- limpo-tour/main_index.js
+++ limpo-tour/main_index.js
@@ -1,6 +1,7 @@
 import './src/layouts/0_page/page.js'
 //*test of option-list styles
 import './src/layouts/6_modal-wins-drop-downs/0_option-list/style/option-list.css'
+import './src/layouts/6_modal-wins-drop-downs/1_expanding-list/style/expanding-list.css'
 //*--------------------------
 import './src/layouts/1_header/header.js'
 import './src/layouts/2_intro/intro.js'
